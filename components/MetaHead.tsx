a7c381ee18fc85d6970a0231336ed1b9447206fd
marcreativo <marcreativo@gmail.com>
2020-03-23 12:22:46 -0300

    styles added and surrounding dependecies

---
+++ components/MetaHead.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default () => (
+  <Head>
+    <title>iúnigo</title>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.iunigo.com/atomic-uikit/index.css"
+    />
+    <link
+      rel="stylesheet"
+      type="text/css"
+      href="https://cdn.iunigo.com/atomic-uikit/themes/blue-themes.css"
+    />
+    <script
+      async
+      src="https://secure.mlstatic.com/sdk/javascript/v1/mercadopago.js"
+    ></script>
+  </Head>
+)
