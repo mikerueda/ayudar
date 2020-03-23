bfbd9d5c19c01efb180c6175bf31884683518685
Mike Rueda <marcreativo@gmail.com>
2020-03-23 12:43:23 -0300

    state manager module (#2)

---
+++ state/reducer.ts
@@ -0,0 +1,10 @@
+import { State } from './types'
+
+export const reducer = {
+  /*
+  toggleContactModal: (state: State) => ({
+    ...state,
+    isContactModalOpen: !state.isContactModalOpen
+  })
+  */
+}
